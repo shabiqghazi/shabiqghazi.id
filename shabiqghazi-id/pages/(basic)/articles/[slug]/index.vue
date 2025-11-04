60310b8edef561e3bb06e8560d7425aca91fd588
Shabiq Ghazi Arkaan <shabiq.ga@bts.id>
2025-11-05 01:23:18 +0700

    fix: adjust container width on article detail'

--- shabiqghazi-id/pages/(basic)/articles/[slug]/index.vue
+++ shabiqghazi-id/pages/(basic)/articles/[slug]/index.vue
@@ -1,34 +1,41 @@
 <template>
-  <div class="md:px-20 px-5 flex flex-col lg:flex-row py-20 gap-20 lg:gap-0">
-    <article class="grow flex flex-col gap-8">
-      <div class="categories text-xs flex gap-4 items-center justify-between">
-        <p class="flex items-center gap-2">
-          <CalendarDays :size="10" />
-          <span>{{ formatDateHumanize(article?.publishedAt ?? "") }}</span>
-        </p>
-        <Badge variant="secondary" class="bg-green-200 text-green-500 text-xs">
-          <Tag />
-          <span>{{ article?.category.name }}</span>
-        </Badge>
+  <div class="px-5 flex flex-col py-32 gap-20 lg:gap-0 items-center">
+    <main
+      class="md:max-w-3xl lg:max-w-4xl xl:max-w-7xl w-full flex flex-col lg:flex-row lg:gap-0 gap-16"
+    >
+      <article class="grow flex flex-col gap-8">
+        <div class="categories text-xs flex gap-4 items-center justify-between">
+          <p class="flex items-center gap-2">
+            <CalendarDays :size="10" />
+            <span>{{ formatDateHumanize(article?.publishedAt ?? "") }}</span>
+          </p>
+          <Badge
+            variant="secondary"
+            class="bg-green-200 text-green-500 text-xs"
+          >
+            <Tag />
+            <span>{{ article?.category.name }}</span>
+          </Badge>
+        </div>
+        <h1 class="text-2xl font-bold text-center">
+          {{ article?.title }}
+        </h1>
+        <NuxtImg
+          v-if="article?.cover.url"
+          :src="getMediaUrl(article.cover.url)"
+          :srcset="getStrapiSrcSet(article.cover.formats)"
+          :alt="article.title"
+          loading="lazy"
+        />
+        <div
+          class="[&>p]:mb-4 ck-content !leading-loose"
+          v-sanitize-html="article?.body"
+        />
+      </article>
+      <div class="flex flex-col">
+        <BaseSidebar />
       </div>
-      <h1 class="text-2xl font-bold text-center">
-        {{ article?.title }}
-      </h1>
-      <NuxtImg
-        v-if="article?.cover.url"
-        :src="getMediaUrl(article.cover.url)"
-        :srcset="getStrapiSrcSet(article.cover.formats)"
-        :alt="article.title"
-        loading="lazy"
-      />
-      <div
-        class="[&>p]:mb-4 ck-content !leading-loose"
-        v-sanitize-html="article?.body"
-      />
-    </article>
-    <div class="flex flex-col">
-      <BaseSidebar />
-    </div>
+    </main>
   </div>
 </template>
 
